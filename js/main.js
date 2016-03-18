2228b1f58c17c5f97530e6eaa9ce3107245d2d3e
Chan0369 <xbetty_chanx@outlook.com>
2016-03-18 18:34:56 -0400

    Added variables in js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+var $input = $('#to-do-list-input');
+var $list = $('.list');
 
+$('form').on('submit', function (e) {
+  var $li = $('<li>');
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('is-clicked');
+});
